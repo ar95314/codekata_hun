69f30217d880b4f73e38227bceed11160fe9d7ff
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-26 15:00:50 +0530

    Update First_rep.py

--- First_rep.py
+++ First_rep.py
@@ -1,10 +1,15 @@
+#ice
 n=int(input())
 x=[i for i in input().split()]
 d=[]
+c=0
 for i in range(0,len(x)):
     if x[i] not in d:
         d.append(x[i])
-    else:
+    elif x[i] in d:
         print(x[i])
+        c+=1
         break
+if c==0:
+    print("unique")
 
