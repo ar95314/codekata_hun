bb554ca047fdd40bcd5b447e6145f54067431cd9
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-26 17:16:33 +0530

    Update SumZero.py

--- SumZero.py
+++ SumZero.py
@@ -1,11 +1,24 @@
+#ganu
 n=int(input())
 l=[int(i) for i in input().split()]
-y=[[l[i],l[j]] for i in range(len(l)) for j in range(len(l)) if i<j if (l[i]+l[j])==0]
-for g in y:
-    for h in range(0,len(g)):
-        if h==0:
-            print(g[h],end="")
-        else:
-            print("",g[h],end="")
-    if len(y)>1:
-        print()
+y=[]
+for i in range(len(l)):
+    for j in range(len(l)):
+        if i<j :
+            if (l[i]+l[j])==0:  
+                y.append(l[i])
+                y.append(l[j])
+                i=i+1
+            elif (l[i]+l[j])==-1:
+                y.append(l[i])
+                y.append(l[j])
+                i=i+1
+            elif (l[i]+l[j])==1:
+                y.append(l[i])  
+                y.append(l[j])
+                i=i+1
+for h in range(0,2):
+    if h==0:
+        print(y[h],end="")
+    else:
+        print("",y[h],end="")
