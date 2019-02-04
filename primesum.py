11c8fb3aac910fbd102a630e14e25444618143b2
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-02-04 19:37:19 +0530

    Create primesum.py

---
+++ primesum.py
@@ -0,0 +1,22 @@
+def isprime(x):
+    c=1
+    for i in range(2,x):
+        if x%i==0:
+            c=0
+            break
+    return c
+n=int(input())
+l=[]
+d=0
+for i in range(2,n):
+    print(isprime(i))
+    if isprime(i)==1:
+        l.append(i)
+print(l)
+l1=l
+for i in l:
+    for j in l1:
+        if (i+j)==n:
+            l1.remove(j)
+            d+=1
+print(d)
