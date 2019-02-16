68a263d05d9b2fb0acc4a4ed6d4fe1db46c76529
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-02-16 09:56:07 +0530

    Update diag_sum.py

--- diag_sum.py
+++ diag_sum.py
@@ -3,5 +3,5 @@ l=[]
 x=[]
 for i in range(n):
     l.append(input().split())
-x=[int(l[i][i]) for i in range(len(l))]
+x=[int(l[i][j]) for i in range(len(l)) for j in range(len(l)) if (i+j)==len(l)-1]
 print(sum(x))
