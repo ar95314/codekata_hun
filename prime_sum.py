45094444f1a0550f600edfbadb24cd8fa6da6f93
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-02-18 20:00:46 +0530

    Create prime_sum.py

---
+++ prime_sum.py
@@ -0,0 +1,10 @@
+def isprime(n):
+	for i in range(2,n):
+		if n%i==0:
+			return 0
+			break
+	else:
+		return 1
+n=int(input())
+x=[[i,j] for i in range(n) for j in range(n) if isprime(i)==1 and isprime(j)==1 and (i+j)==n]
+print(*x[0])
