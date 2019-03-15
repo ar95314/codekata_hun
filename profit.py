842e979f6994d40d7897f1aadabeb773d40fab18
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-03-15 19:45:58 +0530

    Update profit.py

--- profit.py
+++ profit.py
@@ -2,4 +2,13 @@ n=int(input())
 x=[int(i) for i in input().split()]
 m1=min(x)
 m2=max(x)
-print(m2-m1)
+if x.count(x[0])==len(x):
+		print(0)
+else:
+	for i in range(len(x)):
+		if x[i]==m1:
+			mi1=i
+		elif x[i]==m2:
+			mi2=i
+	if mi1<mi2:
+		print(m2-m1)
